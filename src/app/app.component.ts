f9512854786e81cc2db9144306ef95ab152b4819
Elizaveta <elizaveta.np@gmail.com>
2022-10-06 16:06:17 +0200

    product and list added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs'
+
+import { IProduct } from './models/product';
+import { ProductService } from './services/products.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chatGeneration';
+export class AppComponent implements OnInit {
+
+  title = 'Boutique en ligne'
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
+
+  constructor(private productsService: ProductService) { }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(tap(() => this.loading = false))
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
+
 }
